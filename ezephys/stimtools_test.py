e7d564b47481e8416a54f2737bd99881233a0248
Emerson Harkin <emerson.f.harkin@gmail.com>
2019-10-23 23:20:07 -0400

    unittest backwards compatibility changes

--- ezephys/stimtools_test.py
+++ ezephys/stimtools_test.py
@@ -85,43 +85,43 @@ class TestConcatenate(unittest.TestCase):
         command1 = np.array([1, 2, 3, 2])
         command2 = np.array([1, 1, 2, 2, 3])
 
-        with self.subTest('Test behaviour with good dt.'):
-            compound1 = st.CompoundStimulus(command1, dt=dt)
-            compound2 = st.CompoundStimulus(command2, dt=dt)
+        # Test behaviour with good dt.
+        compound1 = st.CompoundStimulus(command1, dt=dt)
+        compound2 = st.CompoundStimulus(command2, dt=dt)
 
-            new_compound = st.concatenate([compound1, compound2])
+        new_compound = st.concatenate([compound1, compound2])
 
-            npt.assert_array_almost_equal(
-                new_compound.command,
-                np.concatenate([command1, command2], axis=-1),
-                err_msg='Concatenatation of vector with CompoundStimulus not equivalent to '
-                'numpy concatenation.'
-            )
-            self.assertEqual(
-                new_compound.no_timesteps,
-                len(command1) + len(command2),
-                'no_timesteps does not match length of commands inputs.'
-            )
-            self.assertEqual(
-                new_compound.dt, dt, 'Incorrect dt in concatenated stimulus.'
-            )
+        npt.assert_array_almost_equal(
+            new_compound.command,
+            np.concatenate([command1, command2], axis=-1),
+            err_msg='Concatenatation of vector with CompoundStimulus not equivalent to '
+            'numpy concatenation.'
+        )
+        self.assertEqual(
+            new_compound.no_timesteps,
+            len(command1) + len(command2),
+            'no_timesteps does not match length of commands inputs.'
+        )
+        self.assertEqual(
+            new_compound.dt, dt, 'Incorrect dt in concatenated stimulus.'
+        )
 
-        with self.subTest('Test behaviour when bad dt is supplied.'):
-            compound1 = st.CompoundStimulus(command1, dt=1)
-            compound2 = st.CompoundStimulus(command2, dt=2)
-            with self.assertRaises(
-                ValueError,
-                msg='Value error not raised for mismatched dt using auto dt.'
-            ):
-                st.concatenate([compound1, compound2])
+        # Test behaviour when bad dt is supplied.
+        compound1 = st.CompoundStimulus(command1, dt=1)
+        compound2 = st.CompoundStimulus(command2, dt=2)
+        with self.assertRaises(
+            ValueError,
+            msg='Value error not raised for mismatched dt using auto dt.'
+        ):
+            st.concatenate([compound1, compound2])
 
-            compound1 = st.CompoundStimulus(command1, dt=dt)
-            compound2 = st.CompoundStimulus(command2, dt=dt)
-            with self.assertRaises(
-                ValueError,
-                msg='Value error not raised for mismatched dt using specified dt.'
-            ):
-                st.concatenate([compound1, compound2], dt=dt*2)
+        compound1 = st.CompoundStimulus(command1, dt=dt)
+        compound2 = st.CompoundStimulus(command2, dt=dt)
+        with self.assertRaises(
+            ValueError,
+            msg='Value error not raised for mismatched dt using specified dt.'
+        ):
+            st.concatenate([compound1, compound2], dt=dt*2)
 
     def test_vectors_only(self):
         """Test concatenation of numpy arrays using stimtools.concatenate."""
@@ -146,12 +146,12 @@ class TestConcatenate(unittest.TestCase):
             new_compound.dt, dt, 'Incorrect dt in concatenated stimulus.'
         )
 
-        with self.subTest('Test behaviour when bad dt is supplied.'):
-            with self.assertRaises(
-                ValueError,
-                msg='Value error not raised when required dt is not supplied.'
-            ):
-                st.concatenate([command1, command2])
+        # Test behaviour when bad dt is supplied.
+        with self.assertRaises(
+            ValueError,
+            msg='Value error not raised when required dt is not supplied.'
+        ):
+            st.concatenate([command1, command2])
 
 
 if __name__ == '__main__':
